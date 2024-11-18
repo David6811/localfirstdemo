7b7e99f4b7b2845608f5f78195ab7782471d0ce5
david <am00s1@163.com>
2024-11-18 17:43:10 +1100

    resolve ReferenceError: window is not defined

---
+++ src/app/components/PowerSyncComponent.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import "./index.css";
+
+const App: React.FC = () => {
+  return (
+    <div >
+      
+    </div>
+  );
+};
+
+export default App;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,11 @@
-"use client";
-import { useEffect, useState } from "react";
+import PowerSyncComponent from "./components/PowerSyncComponent";
 import styles from "./page.module.css";
-import "./index.css";
-import { User } from "./domain/data/models/User";
-import { findUsers, setupPowerSync } from "./services/powersync";
 
 export default function Home() {
-  const [data, setData] = useState<User[] | null>(null);
-
-  useEffect(() => {
-    const initPowerSync = async () => {
-      await setupPowerSync();
-      const users = await findUsers();
-      console.log("Users:", users);
-      setData(users);
-    };
-
-    initPowerSync();
-  }, []);
+  
   return (
     <div className={styles.page}>
-      <div className="customer-list">
-        {data && data.length > 0 ? (
-          <ul>
-            {data.map((item) => (
-              <li key={item.id} className="list-item">
-                <span className="item-name">{item.id}</span>
-                <span className="item-name">{item.name}</span>
-                <span className="item-email">{item.email}</span>
-                <span className="item-id">{item.age}</span>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <span>No data available</span>
-        )}
-      </div>
+      <PowerSyncComponent />
     </div>
   );
 }
