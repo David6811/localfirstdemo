3c67127ac554c2deb742a35117ed513cfcd3da03
David <86584824+David6811@users.noreply.github.com>
2024-11-18 22:53:33 +1100

    Auth0 (#2)
    * npm install @auth0/nextjs-auth0
    
    * add dynamic API route handler
    
    * resolve ReferenceError: window is not defined
    
    * resolve SSR issue by dynamically import component to disable SSR
    
    * add the UserProvider component
    
    * add login and logout link add disable no-html-link-for-pages warnning in eslintrc
    
    * Add profile
    
    * config AUTH0_AUDIENCE to enable to generate JWT with payload
    
    * change PowerSync addr config
    
    * add .env.local into git

---
+++ src/app/components/PowerSyncComponent.tsx
@@ -0,0 +1,50 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import "./index.css";
+import { findUsers, setupPowerSync } from "../services/powersync";
+import { User } from "../domain/data/models/User";
+import ProfileClient from "./Profile/ProfileClient";
+
+const App: React.FC = () => {
+  const [data, setData] = useState<User[] | null>(null);
+
+  useEffect(() => {
+    const initPowerSync = async () => {
+      await setupPowerSync();
+      const users = await findUsers();
+      console.log("Users:", users);
+      setData(users);
+    };
+
+    initPowerSync();
+  }, []);
+  return (
+    <div>
+      <ProfileClient />
+      <div className="customer-list">
+        {data && data.length > 0 ? (
+          <ul>
+            {data.map((item) => (
+              <li key={item.id} className="list-item">
+                <span className="item-name">{item.id}</span>
+                <span className="item-name">{item.name}</span>
+                <span className="item-email">{item.email}</span>
+                <span className="item-id">{item.age}</span>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <span>No data available</span>
+        )}
+      </div>
+      <div>
+        <a href="/api/auth/login">Login</a>
+      </div>
+      <div>
+        <a href="/api/auth/logout">Logout</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
